ea502a43208b0e63245c1e98886f18f5a9c77540
MigueGQ <gq0392032021@unab.edu.sv>
2021-11-08 21:54:23 -0600

    v1

---
+++ Parcial02/Parcial02/Controllers/CajeroController.cs
@@ -0,0 +1,52 @@
+﻿using Parcial02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial02.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult IndexCajero()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Incorrecto()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult IndexCajero(ClsCajero caj)
+        {
+
+            if (caj.monto % 5 == 0)
+            {
+                return Redirect("Correcto");
+            }
+            else
+            {
+                return Redirect("Incorrecto");
+            }
+            return View();
+
+        }
+    }
+}
